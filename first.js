9e9e0d85151294131e0465a9c6b8cbe1be79cea4
Vigneshetm <97948028+Vigneshetm@users.noreply.github.com>
2022-11-08 20:19:16 +0530

    js

---
+++ first.js
@@ -0,0 +1,25 @@
+document.getElementById("demo").innerHTML ="The value of z is: " + z;
+function user()
+{
+var u=document.getElementById('username').value;
+var p=document.getElementById('password').value;
+if(u=='abi'&&p=='udhaya')
+{
+document.getElementById('p2').innerHTML='valid';
+}
+else
+{
+document.getElementById('p2').innerHTML='unvalid';
+}
+for (var i=1; i<5; i++){
+    console.log(i);
+}
+var a = 0;
+while(a<5){
+    console.log(a);
+    a++;
+}
+}
+function oddeven(){
+
+}
